61365fcaf2c3b88aa84e0eaee48369d1764f1fa0
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-08-10 11:41:22 -0700

    Add database setup script

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,18 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+drop table if exists posts;
+drop table if exists github_users;
+
+create table github_users (
+    id bigint generated always as identity primary key,
+    username varchar(255) not null unique,
+    email varchar(320),
+    avatar text
+);
+
+create table posts (
+    id bigint generated always as identity primary key,
+    author_id bigint,
+    content varchar(255) not null,
+    foreign key(author_id) references github_users(id) on delete set null
+)
