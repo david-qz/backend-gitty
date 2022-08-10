9fc42d4bac279333f770b7f9463cb9f4b6c6f97f
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-08-10 12:40:29 -0700

    Implement /login redirect

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,15 +1,25 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
 
-describe('backend-express-template routes', () => {
+const redirectLocationPattern = new RegExp(
+  `https://github\\.com/login/oauth/authorize\\?client_id=\\w+&scope=user&redirect_uri=${process.env.GH_REDIRECT_URI}`,
+  'i'
+);
+
+describe('gitty routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+
+  it('GET /api/v1/github/login should redirect to github oauth page', async () => {
+    const res = await request(app).get('/api/v1/github/login');
+
+    expect(res.redirect).toEqual(true);
+    expect(res.header.location).toMatch(redirectLocationPattern);
   });
+
   afterAll(() => {
     pool.end();
   });
