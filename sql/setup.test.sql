2115638e7235c45086aa2ce4c402d975269902f5
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-08-10 21:30:30 -0700

    Make setup.test.sql run in addition to setup.sql rather than instead
    Now we don't have to repeat the tables creation just to add dummy data
    for the tests.

--- sql/setup.test.sql
+++ sql/setup.test.sql
@@ -1,22 +1,5 @@
--- Use this file to define your SQL tables
--- The SQL in this file will be executed when you run `npm run setup-db`
-drop table if exists posts;
-drop table if exists github_users;
-
-create table github_users (
-    id bigint generated always as identity primary key,
-    username varchar(255) not null unique,
-    email varchar(320),
-    avatar text
-);
-
-create table posts (
-    id bigint generated always as identity primary key,
-    author_id bigint,
-    content varchar(255) not null,
-    foreign key(author_id) references github_users(id) on delete set null
-);
-
+-- Use this file to add dummy data during testing
+-- This file is run after setup.sql only in a test environment
 insert into github_users (username, email, avatar)
 values
 ('bob', 'bob@example.com', 'bobs-avatar.png'),
