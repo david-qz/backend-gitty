432b2aae8476afd9f899b1a0ec6918a5b475d168
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-08-10 18:07:06 -0700

    Implement POST /api/v1/posts
    Users will automatically have authorship added to the new post using
    their session token.

--- __tests__/posts.test.js
+++ __tests__/posts.test.js
@@ -3,6 +3,7 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
+// The mocked authorize middleware will make use appear as user bob to all protected routes.
 jest.mock('../lib/middleware/authorize');
 
 describe('gitty posts routes', () => {
@@ -26,6 +27,24 @@ describe('gitty posts routes', () => {
         avatar: expect.any(String)
       },
       content: expect.any(String)
+    });
+  });
+
+  it('POST /api/v1/posts should make a new post', async () => {
+    const res = await request(app).post('/api/v1/posts').send({ content: 'test post' });
+    expect(res.status).toEqual(200);
+
+    const post = res.body;
+
+    expect(post).toEqual({
+      id: expect.any(String),
+      content: 'test post',
+      author: {
+        id: 1,
+        username: 'bob',
+        email: 'bob@example.com',
+        avatar: 'bobs-avatar.png'
+      }
     });
   });
 
